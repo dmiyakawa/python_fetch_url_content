4af1ee5c484b550b17cd8581f2ec5ad7a04c7cad
Daisuke Miyakawa <dmiyakawa@mokha.co.jp>
2017-03-31 11:31:06 +0900

    urllib3本体から出てくる警告を抑制する

--- fetch_url_content.py
+++ fetch_url_content.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright [2017] [Daisuke Miyakawa]
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,6 +21,7 @@ from logging import getLogger, StreamHandler, Formatter
 from logging import DEBUG
 import platform
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import sys
 import traceback
 
@@ -58,6 +59,8 @@ def main():
     logger.debug('Start running (url: {}, Python-Version: {})'
                  .format(args.url, platform.python_version()))
     try:
+        if not args.verify:
+            requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
         r = requests.get(args.url, verify=args.verify)
     except Exception as e:
         logger.error('Exception raised during fetching content ({})'
